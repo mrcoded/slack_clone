d8df7d3c91028bac608f664dd7d6f884ce54d8f1
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-15 00:38:24 +0100

    feat: configured auth flows providers and middleware for authorization

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,5 +1,20 @@
 import { convexAuth } from "@convex-dev/auth/server";
+import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
+import { Password } from "@convex-dev/auth/providers/Password";
+
+import { DataModel } from "./_generated/dataModel";
+
+//to make the name field filled
+const CustomPassword = Password<DataModel>({
+  profile(params) {
+    return {
+      email: params.email as string,
+      name: params.name as string,
+    };
+  },
+});
 
 export const { auth, signIn, signOut, store } = convexAuth({
-  providers: [],
+  providers: [CustomPassword, GitHub, Google],
 });
