f2825e698a8da6db941e18d48764b8ca5967acf4
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-20 00:26:15 +0100

    feat: created a layout for workspaceId to contain the toolbar, sidebar, and resizable workspacesidebar components

---
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+
+import Toolbar from "../shared/toolbar";
+import Sidebar from "../shared/sidebar";
+
+import WorkspaceSidebar from "./_components/workspace-sidebar";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+interface WorkspaceIdLayoutProps {
+  children: React.ReactNode;
+}
+
+const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  return (
+    <div className="h-full">
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+            <div>Channels Sidebar</div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </div>
+  );
+};
+
+export default WorkspaceIdLayout;
