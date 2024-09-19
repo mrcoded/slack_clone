6300cbe5a89f1f85f3641d8431dde609f10fe695
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-20 00:16:06 +0100

    feat: created a sidebar component

---
+++ src/app/workspace/shared/sidebar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { usePathname } from "next/navigation";
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react";
+
+import UserButton from "@/app/auth/_components/user-button";
+
+import WorkspaceSwitcher from "../[workspaceId]/_components/workspace-switcher";
+import SidebarButton from "../[workspaceId]/_components/sidebar-button";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
+      <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton icon={MessageSquare} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
+      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+        <UserButton />
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
