86b692c3ac913e0c9deb46ab657051483d3023e5
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-20 00:03:18 +0100

    feat: added members, users and workspaces api to convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -17,6 +17,9 @@ import type {
 } from "convex/server";
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
+import type * as members from "../members.js";
+import type * as users from "../users.js";
+import type * as workspaces from "../workspaces.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -29,6 +32,9 @@ import type * as http from "../http.js";
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   http: typeof http;
+  members: typeof members;
+  users: typeof users;
+  workspaces: typeof workspaces;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
