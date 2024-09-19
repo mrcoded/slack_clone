1d364bfa38e3c3330146f8c07b7d59a3840435ae
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-20 00:00:46 +0100

    feat: created a get current member api route and actions

---
+++ convex/members.ts
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+
+import { auth } from "./auth";
+import { query } from "./_generated/server";
+
+export const current = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    //dont throw erros in queries, only in mutations
+    if (userId === null) {
+      return null;
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
+    }
+
+    return member;
+  },
+});
