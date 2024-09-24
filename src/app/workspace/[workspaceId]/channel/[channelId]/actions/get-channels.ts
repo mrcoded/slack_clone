7f1d9d54387277c5304ab0cbc1ad7552ce8bf927
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-24 23:23:12 +0100

    chore

--- src/app/workspace/[workspaceId]/channel/[channelId]/actions/get-channels.ts
+++ src/app/workspace/[workspaceId]/channel/[channelId]/actions/get-channels.ts
@@ -1,5 +1,4 @@
-import { useQuery } from "@/../convex/react";
-
+import { useQuery } from "convex/react";
 import { api } from "@/../convex/_generated/api";
 import { Id } from "@/../convex/_generated/dataModel";
 
