940152fa95cc4853fc35b075ad8ff6dd2d356772
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-28 23:12:15 +0100

    feat: built a get-message actions

---
+++ src/app/messages/actions/get-messages.ts
@@ -0,0 +1,30 @@
+import { usePaginatedQuery } from "convex/react";
+import { paginationOptsValidator } from "convex/server";
+
+import { api } from "@/../convex/_generated/api";
+import { Id } from "@/../convex/_generated/dataModel";
+
+const BATCH_SIZE = 20;
+
+interface getMessagesProps {
+  channelId?: Id<"channels">;
+  parentMessageId?: Id<"messages">;
+  conversationId?: Id<"conversations">;
+}
+
+export type GetMessagesReturnType =
+  (typeof api.messages.get._returnType)["page"];
+
+export const getMessages = ({
+  channelId,
+  parentMessageId,
+  conversationId,
+}: getMessagesProps) => {
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.messages.get,
+    { channelId, parentMessageId, conversationId },
+    { initialNumItems: BATCH_SIZE }
+  );
+
+  return { results, status, loadMore: () => loadMore(BATCH_SIZE) };
+};
