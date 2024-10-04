1fc594f98df85ff4667bb760d7c947f733ae07a4
CodedLibra <adeola.ogundijo@yahoo.com>
2024-10-04 19:20:01 +0100

    feat: added the handleUpdate and hahndleDelete functions, and the Editor component if isEditing is true and a confirmdialog for delete actions

--- src/app/messages/_components/message.tsx
+++ src/app/messages/_components/message.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import dynamic from "next/dynamic";
 
 import { Doc, Id } from "@/../convex/_generated/dataModel";
-
 import { format, isToday, isYesterday } from "date-fns";
+import { toast } from "sonner";
+
+import { updateMessage } from "../actions/update-message";
+import { removeMessage } from "../actions/remove-message";
+
+import MessageToolbar from "./message-toolbar";
 import Hint from "@/components/hint";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Thumbnail from "@/components/thumbnail";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { useConfirm } from "@/hooks/use-confirm";
+
 const Renderer = dynamic(() => import("@/app/messages/_components/renderer"), {
+  ssr: false,
+});
+
+const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
 });
 
@@ -65,59 +77,174 @@ const Message = ({
   repliesImage,
   repliesTimestamp,
 }: MessageProps) => {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete message",
+    "Are you sure you want to delete this message? This cannot be undone."
+  );
+
+  const { mutate: updatingMessage, isPending: isUpdatingMessage } =
+    updateMessage();
+  const { mutate: removingMessage, isPending: isRemovingMessage } =
+    removeMessage();
+
+  const isPending = isUpdatingMessage;
+
+  const handleUpdate = ({ body }: { body: string }) => {
+    updatingMessage(
+      { id, body },
+      {
+        onSuccess: () => {
+          toast.success("Message updated");
+          setEditingId(null);
+        },
+        onError: () => {
+          toast.error("Failed to update message");
+        },
+      }
+    );
+  };
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
+    removingMessage(
+      { id },
+      {
+        onSuccess: () => {
+          toast.success("Message deleted");
+          setEditingId(null);
+        },
+        onError: () => {
+          toast.error("Failed to delete message");
+        },
+      }
+    );
+  };
+
   if (isCompact) {
     return (
-      <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
-        <div className="flex items-start gap-2">
-          <Hint label="">
-            <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100 w-[40px] leading-[22px] text-center hover:underline">
-              {format(Date.now(), "hh:mm")}
-            </button>
-          </Hint>
-          <div className="flex flex-col w-full">
-            <Renderer value={body} />
-            <Thumbnail url={image} />
+      <>
+        <ConfirmDialog />
+        <div
+          className={cn(
+            "flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative",
+            isEditing && "bg-[#f2c74433] hover:bg-[#f2c74433]",
+            isRemovingMessage &&
+              "bg-rose-500/50 transform transition-all scale-y-0 origin-bottom duration-200"
+          )}
+        >
+          <div className="flex items-start gap-2">
+            <Hint label="">
+              <button className="text-xs text-muted-foreground opacity-0 group-hover:opacity-100 w-[40px] leading-[22px] text-center hover:underline">
+                {format(Date.now(), "hh:mm")}
+              </button>
+            </Hint>
+            {isEditing ? (
+              <div className="w-full h-full">
+                <Editor
+                  onSubmit={handleUpdate}
+                  disabled={isPending}
+                  defaultValue={JSON.parse(body)}
+                  onCancel={() => setEditingId(null)}
+                  variant="update"
+                />
+              </div>
+            ) : (
+              <div className="flex flex-col w-full">
+                <Renderer value={body} />
+                <Thumbnail url={image} />
 
-            {updatedAt ? (
-              <span className="text-xs text-muted-foreground">(edited)</span>
-            ) : null}
+                {updatedAt ? (
+                  <span className="text-xs text-muted-foreground">
+                    (edited)
+                  </span>
+                ) : null}
+              </div>
+            )}
           </div>
+
+          {!isEditing && (
+            <MessageToolbar
+              isAuthor={isAuthor}
+              isPending={isPending}
+              handleEdit={() => setEditingId(id)}
+              handleReply={() => {}}
+              handleDelete={handleDelete}
+              handleReaction={() => {}}
+              hideRepliesButton={hideRepliesButton}
+            />
+          )}
         </div>
-      </div>
+      </>
     );
   }
 
   return (
-    <div className="flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative">
-      <div className="flex items-start gap-2">
-        <button>
-          <Avatar>
-            <AvatarImage src={authorImage} />
-            <AvatarFallback>
-              {authorName.charAt(0).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        </button>
-        <div className="flex flex-col w-full overflow-hidden">
-          <div className="text-sm">
-            <button className="font-bold text-primary hover:underline">
-              {authorName}
-            </button>
-            <span>&nbsp;&nbsp;</span>
-            <Hint label="">
-              <button className="text-xs text-muted-foreground hover:underline">
-                {format(Date.now(), "h:mm a")}
-              </button>
-            </Hint>
-          </div>
-          <Renderer value={body} />
-          <Thumbnail url={image} />
-          {updatedAt ? (
-            <span className="text-xs text-muted-foreground">(edited)</span>
-          ) : null}
+    <>
+      <ConfirmDialog />
+      <div
+        className={cn(
+          "flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative",
+          isEditing && "bg-[#f2c74433] hover:bg-[#f2c74433]",
+          isRemovingMessage &&
+            "bg-rose-500/50 transform transition-all scale-y-0 origin-bottom duration-200"
+        )}
+      >
+        <div className="flex items-start gap-2">
+          <button>
+            <Avatar>
+              <AvatarImage src={authorImage} />
+              <AvatarFallback>
+                {authorName.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </button>
+          {isEditing ? (
+            <div className="w-full h-full">
+              <Editor
+                onSubmit={handleUpdate}
+                disabled={isPending}
+                defaultValue={JSON.parse(body)}
+                onCancel={() => setEditingId(null)}
+                variant="update"
+              />
+            </div>
+          ) : (
+            <div className="flex flex-col w-full overflow-hidden">
+              <div className="text-sm">
+                <button className="font-bold text-primary hover:underline">
+                  {authorName}
+                </button>
+                <span>&nbsp;&nbsp;</span>
+                <Hint label="">
+                  <button className="text-xs text-muted-foreground hover:underline">
+                    {format(Date.now(), "h:mm a")}
+                  </button>
+                </Hint>
+              </div>
+              <Renderer value={body} />
+              <Thumbnail url={image} />
+              {updatedAt ? (
+                <span className="text-xs text-muted-foreground">(edited)</span>
+              ) : null}
+            </div>
+          )}
         </div>
+        {!isEditing && (
+          <MessageToolbar
+            isAuthor={isAuthor}
+            isPending={isPending}
+            handleEdit={() => setEditingId(id)}
+            handleReply={() => {}}
+            handleDelete={handleDelete}
+            handleReaction={() => {}}
+            hideRepliesButton={hideRepliesButton}
+          />
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
