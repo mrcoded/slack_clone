19b0e86f82eac7436b102f90b78b8b00dc04578d
CodedLibra <adeola.ogundijo@yahoo.com>
2024-10-09 00:04:21 +0100

    built a direct messaging conversation page for members

---
+++ src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { AlertTriangle, Loader } from "lucide-react";
+
+import { Id } from "@/../convex/_generated/dataModel";
+import { toast } from "sonner";
+import useMemberId from "@/hooks/use-member";
+import useWorkspaceId from "@/hooks/use-workspace-id";
+
+import { createOrGetConversation } from "@/api/conversations/create-or-get-conversation";
+import Conversation from "./_components/conversation";
+
+const MemberIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const memberId = useMemberId();
+
+  const [conversationId, setConversationId] =
+    useState<Id<"conversations"> | null>(null);
+
+  const { mutate, isPending } = createOrGetConversation();
+
+  useEffect(() => {
+    mutate(
+      {
+        workspaceId,
+        memberId,
+      },
+      {
+        onSuccess(data) {
+          setConversationId(data);
+        },
+        onError() {
+          toast.error("Failed to create or get conversation");
+        },
+      }
+    );
+  }, [memberId, workspaceId, mutate]);
+
+  if (isPending) {
+    return (
+      <div className="flex flex-col gap-y-2 h-full items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!conversationId) {
+    return (
+      <div className="flex flex-col gap-y-2 items-center justify-center">
+        <AlertTriangle className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Conversation not found
+        </span>
+      </div>
+    );
+  }
+
+  return <Conversation id={conversationId} />;
+};
+
+export default MemberIdPage;
