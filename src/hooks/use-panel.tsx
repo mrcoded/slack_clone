9964de88b39b6a68f5adf6c61b41f697a4ce8b1d
CodedLibra <adeola.ogundijo@yahoo.com>
2024-10-12 15:39:09 +0100

    feat: added use-profile-id to usepanel so that the panel triggers onOpenProfile

--- src/hooks/use-panel.tsx
+++ src/hooks/use-panel.tsx
@@ -1,19 +1,30 @@
+import { useProfileId } from "@/store/use-profile-id";
 import { useThreadId } from "@/store/use-thread-id";
 
 export const usePanel = () => {
   const [threadId, setThreadId] = useThreadId();
+  const [profileId, setProfileId] = useProfileId();
+
+  const onOpenProfile = (memberId: string) => {
+    setProfileId(memberId);
+    setThreadId(null);
+  };
 
   const onOpenMessage = (messageId: string) => {
     setThreadId(messageId);
+    setProfileId(null);
   };
 
   const onClose = () => {
     setThreadId(null);
+    setProfileId(null);
   };
 
   return {
     threadId,
+    profileId,
     onOpenMessage,
+    onOpenProfile,
     onClose,
   };
 };
