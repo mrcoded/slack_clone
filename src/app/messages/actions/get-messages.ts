4df0824c1354fdfbe48a394dcba44fddbc83cd02
CodedLibra <adeola.ogundijo@yahoo.com>
2024-10-26 23:46:43 +0100

    fix: fixed typescipt errors

--- src/app/messages/actions/get-messages.ts
+++ src/app/messages/actions/get-messages.ts
@@ -1,29 +1,39 @@
-import { usePaginatedQuery } from "convex/react";
-import { paginationOptsValidator } from "convex/server";
+import {
+  PaginatedQueryReference,
+  UsePaginatedQueryResult,
+  usePaginatedQuery,
+} from "convex/react";
+import { PaginationResult } from "convex/server";
 
 import { api } from "@/../convex/_generated/api";
 import { Id } from "@/../convex/_generated/dataModel";
 
 const BATCH_SIZE = 5;
 
-interface getMessagesProps {
+interface GetMessagesProps {
   channelId?: Id<"channels">;
   parentMessageId?: Id<"messages">;
   conversationId?: Id<"conversations">;
 }
 
-export type GetMessagesReturnType = (typeof api.messages.get._returnType)["page"];
+export type GetMessagesReturnType = PaginationResult<
+  typeof api.messages.get._returnType
+>;
 
 export const getMessages = ({
   channelId,
   parentMessageId,
   conversationId,
-}: getMessagesProps) => {
+}: GetMessagesProps) => {
   const { results, status, loadMore } = usePaginatedQuery(
-    api.messages.get,
+    api.messages.get as PaginatedQueryReference,
     { channelId, parentMessageId, conversationId },
     { initialNumItems: BATCH_SIZE }
   );
 
-  return { results, status, loadMore: () => loadMore(BATCH_SIZE) };
+  return {
+    results,
+    status,
+    loadMore: () => loadMore(BATCH_SIZE),
+  };
 };
