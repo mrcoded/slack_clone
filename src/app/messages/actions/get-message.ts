04ffcda2a65b016371256f0009280aa6470f39bd
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-13 15:32:36 +0100

    refactor: explicitly defined the types and cleaned up file

--- src/app/messages/actions/get-message.ts
+++ src/app/messages/actions/get-message.ts
@@ -1,15 +1,48 @@
 import { useQuery } from "@/../convex/react";
 
 import { api } from "@/../convex/_generated/api";
-import { Id } from "@/../convex/_generated/dataModel";
+import { Id, Doc } from "@/../convex/_generated/dataModel";
 
 interface getMessageProps {
   id: Id<"messages">;
 }
 
+type Message = {
+  id: Id<"messages">;
+  _id: Id<"messages">;
+  memberId: any;
+  _creationTime: number;
+  user: {
+    _id: string;
+    name: string;
+    image: string;
+  };
+  authorImage?: string;
+  authorName?: string;
+  isAuthor: boolean;
+  reactions: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberIds: Id<"members">[];
+    }
+  >;
+  body: Doc<"messages">["body"];
+  image: string | null | undefined;
+  updatedAt: Doc<"messages">["updatedAt"];
+  createdAt: Doc<"messages">["_creationTime"];
+  isEditing: boolean;
+  setEditingId: (id: Id<"messages"> | null) => void;
+  isCompact?: boolean;
+  hideThreadsButton?: boolean;
+  threadsCount?: number;
+  threadsImage?: string;
+  threadName?: string;
+  threadTimestamp?: number;
+};
+
 export const getMessage = ({ id }: getMessageProps) => {
   const data = useQuery(api.messages.getById, { id });
   const isLoading = data === undefined;
 
-  return { data, isLoading };
+  return { data: data as unknown as Message, isLoading };
 };
--- src/app/messages/actions/get-messages.ts
+++ src/app/messages/actions/get-messages.ts
@@ -6,7 +6,7 @@ import {
 import { PaginationResult } from "convex/server";
 
 import { api } from "@/../convex/_generated/api";
-import { Id } from "@/../convex/_generated/dataModel";
+import { Id, Doc } from "@/../convex/_generated/dataModel";
 
 const BATCH_SIZE = 5;
 
@@ -16,15 +16,50 @@ interface GetMessagesProps {
   conversationId?: Id<"conversations">;
 }
 
-export type GetMessagesReturnType = PaginationResult<
-  typeof api.messages.get._returnType
->;
+interface Message {
+  id: Id<"messages">;
+  _id: Id<"messages">;
+  _creationTime: number;
+  memberId: any;
+  user: {
+    _id: string;
+    name: string;
+    image: string;
+  };
+  authorImage?: string;
+  authorName?: string;
+  isAuthor: boolean;
+  reactions: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberIds: Id<"members">[];
+    }
+  >;
+  body: Doc<"messages">["body"];
+  image: string | null | undefined;
+  updatedAt: Doc<"messages">["updatedAt"];
+  createdAt: Doc<"messages">["_creationTime"];
+  isEditing: boolean;
+  setEditingId: (id: Id<"messages"> | null) => void;
+  isCompact?: boolean;
+  hideThreadsButton?: boolean;
+  threadsCount?: number;
+  threadsImage?: string;
+  threadName?: string;
+  threadTimestamp?: number;
+}
+
+export type GetMessagesReturnType = Message[];
 
 export const getMessages = ({
   channelId,
   parentMessageId,
   conversationId,
-}: GetMessagesProps) => {
+}: GetMessagesProps): {
+  results: GetMessagesReturnType;
+  status: UsePaginatedQueryResult<GetMessagesReturnType>["status"];
+  loadMore: () => void;
+} => {
   const { results, status, loadMore } = usePaginatedQuery(
     api.messages.get as PaginatedQueryReference,
     { channelId, parentMessageId, conversationId },
@@ -32,7 +67,7 @@ export const getMessages = ({
   );
 
   return {
-    results,
+    results: results as GetMessagesReturnType,
     status,
     loadMore: () => loadMore(BATCH_SIZE),
   };
