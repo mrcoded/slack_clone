ad61c35d8ceda95b092c100b11fbc4aa5bb7e751
CodedLibra <adeola.ogundijo@yahoo.com>
2026-02-23 20:36:04 +0100

    feat: created a checkEmailExists function to handle duplicate user emails

--- convex/users.ts
+++ convex/users.ts
@@ -1,15 +1,28 @@
-import { auth } from "./auth";
+import { v } from "convex/values";
 import { query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
 
 export const current = query({
   args: {},
   handler: async (ctx) => {
-    const userId = await auth.getUserId(ctx);
+    const userId = await getAuthUserId(ctx);
 
     if (userId === null) {
       return null;
     }
 
     return await ctx.db.get(userId);
+  },
+});
+
+export const checkEmailExists = query({
+  args: { email: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_email", (q) => q.eq("email", args.email))
+      .unique();
+
+    return !!user; // Returns true if user exists, false otherwise
   },
 });
